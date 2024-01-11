788a841364a68c72613f07488a3ff144adff8ecc
Tbthanh <thanhtb2003ltv@gmail.com>
2024-01-11 13:28:42 +0700

    Adding some stuff

---
+++ Lab5_Traficlight/Inc/SysTick.h
@@ -0,0 +1,13 @@
+/*
+ * SysTaoick.h
+ *
+ *  Created on: Jan 10, 2024
+ *      Author: TBTHANH
+ */
+
+#ifndef SYSTICK_H_
+#define SYSTICK_H_
+
+
+
+#endif /* SYSTICK_H_ */
---
+++ lab05_trafic_light/main.c
@@ -0,0 +1,13 @@
+/*
+// main.c for trafic light
+// Runs on LM4F120/TM4C123
+// Use a table implementation of a Moore finite state machine to operate
+// a traffic light.
+// Tran Ba Thanh
+// 01 - 01 -2024
+*/
+
+int main(void)
+{
+	for(;;);
+}
