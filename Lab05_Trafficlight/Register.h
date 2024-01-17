a7e2485f4b594387d63f3bf8ea194c5507fc591b
Tbthanh <thanhtb2003ltv@gmail.com>
2024-01-17 09:21:26 +0700

    Delete all GPIOx define in Register.h

--- Lab05_Trafficlight/Register.h
+++ Lab05_Trafficlight/Register.h
@@ -16,39 +16,6 @@
 #define GPIOA_BASE			(0x40010800)
 #define GPIOB_BASE			(0x40010C00)
 
-// By using struct and define the *pointer like below, we kinda donot need this part
-/*
-// GPIO A registers (ref: RM0008 9.2)
-#define GPIOA_CRL				((uint32_t *)(GPIO_A_BASE + 0x00))
-#define GPIOA_CRH				((uint32_t *)(GPIO_A_BASE + 0x04))
-#define GPIOA_IDR				((uint32_t *)(GPIO_A_BASE + 0x08))
-#define GPIOA_ODR				((uint32_t *)(GPIO_A_BASE + 0x0C))
-#define GPIOA_BSRR			((uint32_t *)(GPIO_A_BASE + 0x10))
-#define GPIOA_BRR				((uint32_t *)(GPIO_A_BASE + 0x14))
-#define GPIOA_LCKR			((uint32_t *)(GPIO_A_BASE + 0x18))
-// GPIO B registers (ref: RM0008 9.2)
-#define GPIOB_CRL				((uint32_t *)(GPIO_B_BASE + 0x00))
-#define GPIOB_CRH				((uint32_t *)(GPIO_B_BASE + 0x04))
-#define GPIOB_IDR				((uint32_t *)(GPIO_B_BASE + 0x08))
-#define GPIOB_ODR				((uint32_t *)(GPIO_B_BASE + 0x0C))
-#define GPIOB_BSRR			((uint32_t *)(GPIO_B_BASE + 0x10))
-#define GPIOB_BRR				((uint32_t *)(GPIO_B_BASE + 0x14))
-#define GPIOB_LCKR			((uint32_t *)(GPIO_B_BASE + 0x18))
-//  RCC clock registers
-#define RCC_CR					((uint32_t *)(RCC_BASE + 0x00))
-#define RCC_CFGR				((uint32_t *)(RCC_BASE + 0x04))
-#define RCC_CIR					((uint32_t *)(RCC_BASE + 0x08))
-#define RCC_APB2RSTR		((uint32_t *)(RCC_BASE + 0x0C))
-#define RCC_APB1RSTR		((uint32_t *)(RCC_BASE + 0x10))
-#define RCC_AHBENR			((uint32_t *)(RCC_BASE + 0x14))
-#define RCC_APB2ENR			((uint32_t *)(RCC_BASE + 0x18))
-#define RCC_APB1ENR			((uint32_t *)(RCC_BASE + 0x1C))
-#define RCC_BDCR				((uint32_t *)(RCC_BASE + 0x20))
-#define RCC_CSR					((uint32_t *)(RCC_BASE + 0x24))
-#define RCC_AHPRSTR			((uint32_t *)(RCC_BASE + 0x28))
-#define RCC_CFGR2				((uint32_t *)(RCC_BASE + 0x2C))
-*/
-
 // RCC structure
 typedef struct
 {
