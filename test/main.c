1a163c356bdfbfa59b554a59ffaf0034969964bd
Tbthanh <thanhtb2003ltv@gmail.com>
2024-01-20 16:31:33 +0700

    Yatta - run fine
    Testting -2 is lab 5, and its runing fine. Except for the output of B04, which is red light of south is still buggy

--- test/main.c
+++ test/main.c
@@ -3,10 +3,24 @@
 #include <system_stm32f10x.h>
 
 void portB_Init(void);
-void delay(void);
+
+// Systick related function
+void SysTick_Init(uint32_t ticks);
+volatile static uint32_t TimeDelay;
+void SysTick_Handler(void);				//Delay 1ms
+void Delay(uint32_t nTime);
+
+// function to set the sysclock to 48Mhz
+void PLLInit(void);
 
 int main(void)
 {
+	// initialize 48 MHz system clock
+	//PLLInit();
+	
+	// systick
+	SysTick_Init(8000);
+	
 	// Enable RCC for GPIO
 	RCC->APB2ENR |= 0x0C;
 
@@ -18,27 +32,80 @@ int main(void)
 		
 	for(;;)
 	{
-		GPIOB->ODR |= 0x00000003;
-		delay();
-		GPIOB->ODR &= 0xFFFFFFFC;
-		delay();
+		GPIOB->ODR |= 0x00000001;
+		Delay(1000);
+		GPIOB->ODR &= 0xFFFFFFFE;
+		Delay(1000);
 	}
 }
 
 void portB_Init(void)
 {
-	// CRL for pin B 4-7 and 0-1
-	GPIOB->CRL &= 0x0000FF00;
-	GPIOB->CRL |= 0x22220022;
-	// CLH for pin B 8-9
-	GPIOB->CRH |= 0x00000011;
-	// Optional for clearing / setting the LED
+	// CRL for pin 0
+	GPIOB->CRL &= 0xFFFFFFF0;
+	GPIOB->CRL |= 0x00000002;
 }
 
-void delay(void)
+// function to set the sysclock to 48Mhz
+void PLLInit(void)
 {
-	for (uint32_t i = 0; i < 1000000; i++)
-	{
-		__asm("NOP");
-	}
+	/* this part follow the intruction of the following guide:
+			https://medium.com/@csrohit/clock-configuration-in-stm32-6a058da220e0
+	*/
+	// Set flash latency
+	// 48MHz = SYSCLK > 24MHz -> one wait state
+	FLASH->ACR |= FLASH_ACR_LATENCY_2;
+	
+	// setting APB,AHB & HSE divisor
+	// read stm32f10x.h and rm0008 103/1136 for more detail
+	RCC->CFGR |= RCC_CFGR_PPRE1_DIV2;
+	RCC->CFGR |= RCC_CFGR_PPRE2_DIV1;
+	RCC->CFGR &= 0xFFFDFFFF;	// (PLLXTPRE=0)
+	
+	// The HSE crystal of my board doesnt work so ...
+	// turn on HSE
+	RCC->CR 	|= RCC_CR_HSEON;
+	while (!(RCC->CR & RCC_CR_HSERDY)) // code get stuck here
+		;		// hse is ready
+	
+	// config PLL
+	RCC->CFGR |= RCC_CFGR_PLLMULL6;
+	RCC->CFGR |= RCC_CFGR_PLLSRC_HSE;
+	RCC->CFGR |= RCC_CR_PLLON;
+	while (!(RCC->CR & RCC_CR_PLLRDY))
+		;		// pll is ready
+	
+	// sellect the pll for coreclock
+	RCC->CFGR  |= RCC_CFGR_SW_PLL;	// 0X02
+	while (!(RCC->CFGR & RCC_CFGR_SWS_PLL))
+		;		// clock is ready 48Mhz
 }
+
+void SysTick_Init(uint32_t ticks)
+{
+	SysTick->CTRL = 0x00000000;	// SysTick_CTRL_DISABLE;
+	SysTick->LOAD = ticks - 1;
+	// set interupt piority of SysTick to least urgency
+	NVIC_SetPriority (SysTick_IRQn, (1UL << __NVIC_PRIO_BITS) - 1UL);
+	SysTick->VAL = 0;	// reset the value
+	// select processor clock
+	SysTick->CTRL  = SysTick_CTRL_CLKSOURCE_Msk |
+                   SysTick_CTRL_TICKINT_Msk   |
+                   SysTick_CTRL_ENABLE_Msk;  /* Enable SysTick IRQ and SysTick Timer */
+	
+}
+
+void SysTick_Handler(void)
+{
+	if(TimeDelay > 0)
+		TimeDelay--;
+}
+
+void Delay(uint32_t nTime)
+{
+	// nTime: specifies the delay time length
+	TimeDelay = nTime;
+	while(TimeDelay != 0)
+		;		// busy wait
+}
+
--- testing_2/main.c
+++ testing_2/main.c
@@ -1,3 +1,45 @@
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @author         : Tran Ba Thanh
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ *Input:
+ * 	A0:Walk
+ * 	A1:West
+ * 	A2:South
+ *
+ *Input value:
+ *	0x0 = 000 = no sensor
+ *	0x1 = 001 = Walk sensor
+ *	0x2 = 010 = West sensor
+ *	0x3 = 011 = Walk and West sensor
+ *	0x4 = 100 = South sensor
+ *	0x5 = 101 = South and Walk sensor
+ *	0x6 = 110 = South and West sensor
+ *	0x7 = 111 = all three sensor
+ *
+ *Priority
+ *	South > West > Walk
+ *
+ *Output:
+ * 	B0: White Walk light
+ * 	B1: Red Walk light
+ * 	B4: R South light
+ * 	B5: Y South light
+ * 	B6: G South light
+ * 	B7: R South light
+ * 	B8: Y South light
+ * 	B9: G South light
+ *
+ *For more info, please visit: https://github.com/Tbthanh/STM32f103c8xx_code_following_ECE319K
+ *FSM and the excel sheet can be found: STM32f103c8xx_code_following_ECE319K/lab05_trafic_light/00_Others/
+ *
+ ******************************************************************************
+ */
+
 #include <stdint.h>
 #include <stm32f10x.h>
 #include <system_stm32f10x.h>
@@ -13,9 +55,12 @@ void flashing(uint32_t time);
 
 // Systick related function
 void SysTick_Init(uint32_t ticks);
-volatile static uint32_t TimeDelay;
-void SysTick_Handler(void);
+void SysTick_Handler(void);				//Delay 1ms
 void Delay(uint32_t nTime);
+volatile static uint32_t TimeDelay;
+
+// function to set the sysclock to 48Mhz
+void PLLInit(void);
 
 // states
 #define jSo		0
@@ -29,11 +74,11 @@ void Delay(uint32_t nTime);
 #define rWa		8
 #define rAl		9
 
-// wait time (*10ns)
-#define GreenTime	1000	//10s
-#define WaitTime	200		//2s
-#define RedTime		100		//1s
-#define WalkGTime	800		//8s
+// wait time (*1ms)
+#define GreenTime	10000		//10s
+#define WaitTime	2000		//2s
+#define RedTime		1000		//1s
+#define WalkGTime	8000		//8s
 
 // state Structure
 struct State
@@ -48,52 +93,74 @@ struct State
 typedef const struct State STyp;
 static const STyp FSM[10]={
 	// jSo : just South
-	{0x0C0,GreenTime,{wSo,wSo,wSo,wSo,jSo,wSo,wSo,wSo}},
+	{0x000000C2,GreenTime,{wSo,wSo,wSo,wSo,jSo,wSo,wSo,wSo}},
 	// wSo : wait South
-	{0x0A0,WaitTime,{rAl,rSo,rSo,rSo,rAl,rSo,rSo,rSo}},
+	{0x000000A2,WaitTime,{rAl,rSo,rSo,rSo,rAl,rSo,rSo,rSo}},
 	// rSo : red South
-	{0x090,RedTime,{rAl,jWa,jWe,jWe,rAl,jWa,jWe,jWe}},
+	{0x00000092,RedTime,{rAl,jWa,jWe,jWe,rAl,jWa,jWe,jWe}},
 	// jWe : just West
-	{0x210,GreenTime,{wWe,wWe,jWe,wWe,wWe,wWe,wWe,wWe}},
+	{0x00000212,GreenTime,{wWe,wWe,jWe,wWe,wWe,wWe,wWe,wWe}},
 	// wWe : wait West
-	{0x110,WaitTime,{rAl,rWe,rAl,rWe,rWe,rWe,rWe,rWe}},
+	{0x00000112,WaitTime,{rAl,rWe,rAl,rWe,rWe,rWe,rWe,rWe}},
 	// rWe : red West
-	{0x090,RedTime,{rAl,jWa,rAl,jWa,jSo,jSo,jSo,jWa}},
+	{0x00000092,RedTime,{rAl,jWa,rAl,jWa,jSo,jSo,jSo,jWa}},
 	// jWa : just Walk
-	{0x090,WalkGTime,{wWa,jWa,wWa,wWa,wWa,wWa,wWa,wWa}},
+	{0x00000091,WalkGTime,{wWa,jWa,wWa,wWa,wWa,wWa,wWa,wWa}},
 	// wWa : wait Walk
-	{0x090,WaitTime,{rAl,rAl,rWa,rWa,rWa,rWa,rWa,rWa}},
+	{0x00000093,WaitTime,{rAl,rAl,rWa,rWa,rWa,rWa,rWa,rWa}},
 	// rWa : red Walk
-	{0x090,RedTime,{rAl,rAl,jWe,jWe,jSo,jSo,jSo,jSo}},
+	{0x00000092,RedTime,{rAl,rAl,jWe,jWe,jSo,jSo,jSo,jSo}},
 	// rAl : red All
-	{0x090,RedTime,{rAl,jWa,jWe,jWe,jSo,jSo,jSo,jSo}}
+	{0x00000092,RedTime,{rAl,jWa,jWe,jWe,jSo,jSo,jSo,jSo}}
 };
 
 int main(void)
 {
+	// initialize 48 MHz system clock
+	//PLLInit();
+	
 	// systick
-	SysTick_Init(9000);
+	SysTick_Init(8000);
 
 	// Enable RCC for GPIO
-	RCC->APB2ENR |= 0x0C;
+	RCC->APB2ENR |= 0x0000000C;
 
 	// Give time to stabilize the clock
-	while(!((RCC->APB2ENR) & 0x0C));
+	while(!(RCC->APB2ENR & 0x0000000C));
 
 	// Initialize GPIOs
 	portA_Init();
 	portB_Init();
 	
+	// Initial state: red All
+	volatile uint8_t state = (uint8_t)rAl;
+	
 	// Input
-	volatile uint32_t input = 0x00;
+	volatile uint32_t input = 0x00000000;
 		
 	for(;;)
 	{
-		GPIOB->ODR |= 0x000003F0\
-		;
-		Delay(100);
-		GPIOB->ODR &= 0xFFFFFFFC;
-		Delay(100);
+		// set the output to the current state
+		GPIOB->ODR = FSM[state].Output;
+
+		// check if wWa
+		if (state == (uint8_t)wWa)
+		{
+			// flash white-red-off
+			flashing(FSM[state].Time);
+		}
+		else
+		{
+			// delay 10ms * FSM[state].Time
+			Delay(FSM[state].Time);
+		}
+
+		// get new input
+		input = (GPIOA->IDR& 0x07);
+		//input = (*GPIO_A_IDR & 0x07);
+
+		// go to next state
+		state = (uint8_t)(FSM[state].Next[input]);
 	}
 }
 
@@ -103,19 +170,21 @@ void portB_Init(void)
 	GPIOB->CRL &= 0x0000FF00;
 	GPIOB->CRL |= 0x66660066;
 	// CLH for pin B 8-9
-	GPIOB->CRH |= 0x00000022;
+	GPIOB->CRH |= 0x00000066;
 	// Cheking all the LED
 	GPIOB->ODR |= 0x000003F3;
-	Delay(100);
+	Delay(8000);
 	GPIOB->ODR &= 0xFFFFFC0C;
+	Delay(8000);
 }
 
 void portA_Init(void)
 {
 	// CRL for pin A 0-2
-	GPIOA->CRL |= (uint32_t)0x00000088;
+	GPIOA->CRL &= 0xFFFFF000;
+	GPIOA->CRL |= 0x00000888;
 	// Pull up pin A 0-2
-	GPIOA->ODR |= (uint32_t)0x00000003;
+	GPIOA->ODR &= 0xFFFFFFF8;
 }
 
 void flashing(uint32_t time)
@@ -144,7 +213,7 @@ void Delay(uint32_t nTime)
 	// nTime: specifies the delay time length
 	TimeDelay = nTime;
 	while(TimeDelay != 0)
-		__asm("nop");		// busy wait
+		;		// busy wait
 }
 
 void SysTick_Init(uint32_t ticks)
@@ -152,11 +221,46 @@ void SysTick_Init(uint32_t ticks)
 	SysTick->CTRL = 0x00000000;	// SysTick_CTRL_DISABLE;
 	SysTick->LOAD = ticks - 1;
 	// set interupt piority of SysTick to least urgency
-	NVIC_SetPriority (SysTick_IRQn, (1<<__NVIC_PRIO_BITS) - 1);
+	NVIC_SetPriority (SysTick_IRQn, (1UL << __NVIC_PRIO_BITS) - 1UL);
 	SysTick->VAL = 0;	// reset the value
 	// select processor clock
-	SysTick->CTRL |= 0x00000004;
-	// enable systick interupt
-	SysTick->CTRL |= SysTick_CTRL_ENABLE;
-	// enable systick;
+	SysTick->CTRL  = SysTick_CTRL_CLKSOURCE_Msk |
+                   SysTick_CTRL_TICKINT_Msk   |
+                   SysTick_CTRL_ENABLE_Msk;  /* Enable SysTick IRQ and SysTick Timer */
+	
+}
+
+// function to set the sysclock to 48Mhz
+// for some reason my HSE cannot be ready so scrap this
+void PLLInit(void)
+{
+	/* this part follow the intruction of the following guide:
+			https://medium.com/@csrohit/clock-configuration-in-stm32-6a058da220e0
+	*/
+	// Set flash latency
+	// 48MHz = SYSCLK > 24MHz -> one wait state
+	FLASH->ACR |= FLASH_ACR_LATENCY_2;
+	
+	// setting APB,AHB & HSE divisor
+	// read stm32f10x.h and rm0008 103/1136 for more detail
+	RCC->CFGR |= RCC_CFGR_PPRE1_DIV2;
+	RCC->CFGR |= RCC_CFGR_PPRE2_DIV1;
+	RCC->CFGR |= RCC_CFGR_PLLXTPRE_HSE;
+	
+	// turn on HSE
+	RCC->CR 	|= RCC_CR_HSEON;
+	while (!(RCC->CR & RCC_CR_HSERDY)) // code get stuck here
+		;		// hse is ready
+	
+	// config PLL
+	RCC->CFGR |= RCC_CFGR_PLLMULL6;
+	RCC->CFGR |= RCC_CFGR_PLLSRC_HSE;
+	RCC->CFGR |= RCC_CR_PLLON;
+	while (!(RCC->CR & RCC_CR_PLLRDY))
+		;		// pll is ready
+	
+	// sellect the pll for coreclock
+	RCC->CFGR  |= RCC_CFGR_SW_PLL;	// 0X02
+	while (!(RCC->CFGR & RCC_CFGR_SWS_PLL))
+		;		// clock is ready 48Mhz
 }
